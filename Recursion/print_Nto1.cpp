098671d27a46974ab96f0c0a55eb2e8164a8b5df
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-01-02 12:09:06 +0530

    Update print_Nto1.cpp

--- Recursion/print_Nto1.cpp
+++ Recursion/print_Nto1.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print_N_to_1 (int N){
+void print_N_to_1 (int N){    //TC:O(N) SC:O(N)
     if(N==0) return;
     cout<<N<<" ";
     print_N_to_1(N-1);
 }
 
-void print_N_to_1_backtracking (int i, int N){
+void print_N_to_1_backtracking (int i, int N){    //TC:O(N) SC:O(N)
     if(i>N) return;
     print_N_to_1_backtracking(i+1, N);
     cout<<i<<" ";
