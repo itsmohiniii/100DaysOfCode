73d50693fc10f97710c10dc8a7c6b8bd9e71c9fb
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-01-02 11:44:58 +0530

    Create print_Nto1.cpp

---
+++ Recursion/print_Nto1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print_N_to_1 (int N){
+    if(N==0) return;
+    cout<<N<<" ";
+    print_N_to_1(N-1);
+}
+
+void print_N_to_1_backtracking (int i, int N){
+    if(i>N) return;
+    print_N_to_1_backtracking(i+1, N);
+    cout<<i<<" ";
+    
+}
+
+int main() {
+    print_N_to_1(7);
+    cout<<endl;
+    print_N_to_1_backtracking(1, 8);
+    cout<<endl;
+    return 0;
+}
