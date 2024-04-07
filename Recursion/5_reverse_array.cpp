4b6466994cfd9d8905c075da9d06062252784491
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-04-07 18:05:45 +0530

    Update 5_reverse_array.cpp

--- Recursion/5_reverse_array.cpp
+++ Recursion/5_reverse_array.cpp
@@ -16,6 +16,13 @@ void reverse_arr_1_pointer(int arr[], int N, int i){  //TC:O(N/2) SC:O(N/2)
     reverse_arr_1_pointer(arr,N, i+1);
 }
 
+//reverse array and return the reversed array
+int* reverse_arr_return(int* arr, int i, int j){
+    if(i>=j) return arr;
+    swap(arr[i], arr[j]);
+    return reverse_arr_return(arr, i+1, j-1);
+}
+
 
 //reverse vector
 void reverse_vector(vector<int>& vec, int i, int j){    //TC:O(N/2) SC:O(N/2)
@@ -47,6 +54,19 @@ int main() {
     reverse_vector(vec,0,vec.size()-1);
     for(int i=0;i<vec.size();i++){
         cout<<vec[i]<<" ";
+    }
+    cout<<endl<<endl;
+
+    
+    int arr[]= {1, 2, 3, 4, 5};
+    int N=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0; i<N;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    int * ans = reverse_arr_return(arr, 0, N-1);
+    for(int i=0; i<N;i++){
+        cout<<ans[i]<<" ";
     }
     cout<<endl;
     
