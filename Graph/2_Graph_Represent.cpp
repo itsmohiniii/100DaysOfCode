f22d66c4ba20d9ba75692497ca2ef882fb68eb10
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-06-21 02:48:32 +0530

    Update 2_Graph_Represent.cpp

--- Graph/2_Graph_Represent.cpp
+++ Graph/2_Graph_Represent.cpp
@@ -6,13 +6,13 @@ m lines (representing edges)
 
 Store:
 1. Adjacency matrix 
-2. List
+2. Adjacency List
 
-ADJACENCY MATRIX WAY:
 0 or 1 based indexing?
 1-based -> adj[n+1][n+1]
 0-based => adj[n][n] 
 
+ADJACENCY MATRIX WAY (undirected graph):
 //TC: O(M) SC:O(N*N)
 int main {
  int n, m;
