6317ad03d3939b498751800f5738d22dbe26f7f2
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-06-21 01:22:07 +0530

    Update 2_Graph_Represent.cpp

--- Graph/2_Graph_Represent.cpp
+++ Graph/2_Graph_Represent.cpp
@@ -1,1 +1,17 @@
+Directed or Undirected graph?
+
+Input:
+n, m (#nodes, #edges)
+m lines (representing edges)
+
+Store:
+1. Adjacency matrix 
+2. List
+
+ADJACENCY MATRIX WAY:
+0 or 1 based indexing?
+1-based -> adj[n+1][n+1]
+0-based => adj[n][n] 
+
+
 
