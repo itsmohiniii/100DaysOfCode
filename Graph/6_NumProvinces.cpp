b02a1328277f1c9a025c27ac1a191b609835942f
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-06-30 12:48:53 +0530

    Create 6_NumProvinces.cpp

---
+++ Graph/6_NumProvinces.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private: 
+	void dfs (int startNode, vector<int> adjLs[ ], int vis[ ]){
+		vis[startNode] = 1;
+		for(auto it : adjLs[startNode]) {
+			if(!vis[it]) {
+				dfs(it, adjLs, vis);
+			}
+}
+}	
+public:
+	int numProvinces(int N, vector<vector<int>> adjMtx) {
+		//convert adjacency Matrix to Adjacency List
+		vector<int> adjLs[N];
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				if(adjMtx[i][j] == 1 && i!=j) { //no self nodes 
+					adjLs[i].push_back(j);
+				}
+			}
+}	
+
+int vis[N] = {0}; //0-based indexing
+int cnt = 0;
+for(int i=0; i<N; i++){
+	if(vis[i]==0){
+		cnt++;
+		dfs(i, adjLs, vis);
+	}
+}
+	return cnt;
+}
+};
+
+int main() {
+	int N=3;
+	vector<vector<int>> adjMtx = { {1,0,1},
+					   {0,1,0},
+					   {1,0,1} };    
+	Solution obj;
+cout<<obj.numProvinces(N, adjMtx);
+return 0;
+		
+}
+
+
