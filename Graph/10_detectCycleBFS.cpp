bcc076386482d516b86819f32ef0a35f74483f0d
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-07-01 16:11:49 +0530

    Update 10_detectCycleBFS.cpp

--- Graph/10_detectCycleBFS.cpp
+++ Graph/10_detectCycleBFS.cpp
@@ -4,33 +4,49 @@
 using namespace std;
 
 class Solution {
-  public: 
-    bool hasCycle(vector<int> adj[], int N) {
+  private: 
+    bool detectUsingBFS(vector<int> adj[], int startNode, int vis[]) {
+      vis[startNode]=1;
       queue<pair<int,int>> q;
-      int vis[N] = {0}; //0-based indexing
-      q.push({0,-1});
+      q.push({startNode,-1});
       while(!q.empty()) {
         int node = q.front().first;
-        int prevnode = q.front().second;
+        int parent = q.front().second;
         q.pop();
         for(auto it : adj[node]) {
-          if (vis[it] && it!=prevnode) return true;
           if(!vis[it]) {
             vis[it]=1;
             q.push({it,node});
+          } else if (it!=parent) { //vis[it]==1
+            return true;
           }
         }
+      }
+      return false;
+    }
+    
+  public: 
+    bool hasCycle(vector<int> adj[], int N) {
+      int vis[N] = {0}; //0-based indexing
+      for(int i=0;i<N;i++) { //Considering graph can have multiple connected components
+          if(!vis[i]){
+              if(detectUsingBFS(adj,i,vis)) return true;
+          }
       }
       return false;
     }
  };
 
 int main() {
-//   int N = 5;
-//   vector<int> adj[]= {{1}, {0, 2, 4}, {1, 3}, {2, 4}, {1, 3}} ; //Output: 1 (true)
+  int N = 5;
+  vector<int> adj[]= {{1}, {0, 2, 4}, {1, 3}, {2, 4}, {1, 3}} ; //Output: 1 (true)
   
-  int N = 4;
-  vector<int> adj[]=  {{}, {2}, {1, 3}, {2}} ; //Output: 0 (false)
+//   int N = 4;
+//   vector<int> adj[]=  {{}, {2}, {1, 3}, {2}} ; //Output: 0 (false)
+  
+//   int N = 4;
+//   //Multiple connected components example
+//   vector<int> adj[]=  {{}, {2,3}, {1, 3}, {1,2}} ; //Output: 1 (true)
   Solution obj;
   cout<<obj.hasCycle(adj, N);
   return 0;
