45c5063e09e381ec65610d80782d60c65914a196
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-07-11 20:12:38 +0530

    Update 14_numEnclaves.cpp

--- Graph/14_numEnclaves.cpp
+++ Graph/14_numEnclaves.cpp
@@ -16,26 +16,14 @@ class Solution {
       queue<pair<int,int>> q;
 
       //TC:O(N*M)
-      for(int j=0;j<m;j++) {
-        if(grid[0][j]==1 && !vis[0][j]) {
-          q.push({0,j});
-          vis[0][j]=1;
+      for(int row=0;row<n;row++) {
+        for(int col=0;col<m;col++) {
+          if((row==0 || row==n-1 || col==0 || col==m-1) && mat[row][col]=='O' && !vis[row][col]) {
+             q.push({row,col});
+             vis[row][col]=1;
+          }
         }
-        if(grid[n-1][j]==1 && !vis[n-1][j]) {
-          q.push({n-1,j});
-          vis[n-1][j]=1;
-        }
-      }
-      for(int i=0;i<n;i++) {
-        if(grid[i][0]==1 && !vis[i][0]) {
-          q.push({i,0});
-          vis[i][0]=1;
-        }
-        if(grid[i][m-1]==1 && !vis[i][m-1]) {
-          q.push({i,m-1});
-          vis[i][m-1]=1;
-        }
-      }
+      } 
       
       int delrow [ ] = {-1,0,1,0};
       int delcol [ ] = {0,1,0,-1};
