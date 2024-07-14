1378dbec10d54e3b3d910ecc297f147fb4306151
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-07-14 23:48:14 +0530

    Update 18_SafeNodesDFS.cpp

--- Graph/18_SafeNodesDFS.cpp
+++ Graph/18_SafeNodesDFS.cpp
@@ -1,6 +1,6 @@
 // Eventual safe states (using Cycle detection technique DFS) 
 // Directed Graph
-
+//Intuition: Any node that is part of a cycle or is connected/incoming (has an incoming edge) to the cycle - can never be a safe node.
 
 #include<bits/stdc++.h>
 using namespace std;
