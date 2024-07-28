52b7048f07ab651ecf0b696eb891b71cb8185fb9
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-07-28 20:12:04 +0530

    Update 2_fibonacci.cpp

--- DP/2_fibonacci.cpp
+++ DP/2_fibonacci.cpp
@@ -1,5 +1,8 @@
-// Fibonacci no. : 0 1 1 2 3 5 8 13 21...
+// Fibonacci no. : 0 1 1 2 3 5 8 13 21... 
+// 0-based indexing
 // Recurrence relation: f(n) = f(n-1) + f(n-2)
+
+//3 steps for converting a recursive soltion to DP solution (using Memoization) 
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -8,14 +11,17 @@ using namespace std;
 // TC:O(N) SC:O(N)+O(N) recursion stack space + dp array
 int fib(int n, vector<int> &dp){  //call by reference
     if(n<=1) return n;
+    //Step3: check whether the subproblem has been previouly solved    
     if(dp[n] != -1) return dp[n];
+    //Step2: store the answer to subproblem in dp array
     return dp[n] = fib(n-1, dp) +fib(n-2, dp);
 }
 
 int main() {
     int n = 5;
-    
-    vector<int> dp(n+1, -1);
+
+    //Step1: declare a dp array or vector (size -> maximum no. of subproblems)
+    vector<int> dp(n+1, -1); //size=n+1 (index  from 0 to n)
     // if array is used instead of vector
     // int dp[n+1];
     // memset(dp, -1, sizeof dp);
