d837384bbfd84b81a1b9275f090a612379845fe3
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-08-05 22:35:08 +0530

    Update 2_BT_Representation.cpp

--- Trees/2_BT_Representation.cpp
+++ Trees/2_BT_Representation.cpp
@@ -1,1 +1,27 @@
+//Represent Binary Tree in C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:    
+  int data;
+  Node* left;
+  Node* right;
+  Node(int val) { //constructor 
+    data = val;
+    left = nullptr;
+    right = NULL;
+  }
+};
+
+int main() {
+  //dynamic memory allocation
+  Node* root = new Node(1);
+  root->left = new Node(2);
+  root->right = new Node(3);
+  root->left->right = new Node(4);   
+  
+  return 0;
+}
 
