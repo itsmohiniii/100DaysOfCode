b8ff5e2fd5e72405792bb913c8311a0756e128ff
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-08-06 19:30:07 +0530

    Create 7_InOrderIterativeTraversal.cpp

---
+++ Trees/7_InOrderIterativeTraversal.cpp
@@ -0,0 +1,74 @@
+//In-order Traversal of Binary Tree (Iterative approach)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+  int data;
+  TreeNode* left;
+  TreeNode* right;
+
+  TreeNode() {
+    data = 0;
+    left = nullptr;
+    right = nullptr;
+  }
+
+  TreeNode(int val) {
+    data = val;
+    left = nullptr;
+    right = nullptr;
+  }
+  
+  TreeNode(int val, TreeNode* le, TreeNode* ri) {
+    data = val;
+    left = le;
+    right = ri;
+  }
+}; 
+
+class Solution {
+public:
+  //TC: O(N) as we traverse N nodes.
+  //SC: O(N) ~ O(H) where H-> height of BT. for stack WC: stack will store N nodes if the tree just has left children. (i.e. skewed tree)
+  //Left Root Right
+  vector<int> inOrderIterative(TreeNode* root) {
+    vector<int> inorder;
+    if(root == NULL) return inorder;
+    
+    stack<TreeNode*> st; //LIFO
+    TreeNode* node = root;
+    
+    while(true) {
+      if(node !=  NULL) {
+        st.push(node); //created auxiliary stack space using stack DS 
+        node = node->left;
+      } 
+      else { // node is NULL
+        if(st.empty()) break;
+        node = st.top();
+        st.pop();
+        inorder.push_back(node->data);
+        node = node->right;
+      }
+    }
+    
+    return inorder;
+  }
+};
+
+int main() {
+  TreeNode* root = new TreeNode(1);
+  root->left = new TreeNode(2);
+  root->right = new TreeNode(3);
+  root->left->left = new TreeNode(4);   
+  root->left->right = new TreeNode(5); 
+  
+  Solution obj;
+  vector<int> ans = obj.inOrderIterative(root);  //Output: 4 2 5 1 3
+  for(int i=0; i<ans.size();i++){
+      cout<<ans[i]<<" ";
+  } 
+  return 0;
+}
+
