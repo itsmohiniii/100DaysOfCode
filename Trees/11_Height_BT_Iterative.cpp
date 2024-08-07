752a553ad50e7d8f79036eedb6db890dcaa42dc1
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-08-07 12:42:37 +0530

    Update 11_Height_BT_Iterative.cpp

--- Trees/11_Height_BT_Iterative.cpp
+++ Trees/11_Height_BT_Iterative.cpp
@@ -1,4 +1,4 @@
-//Maximum Depth or Height of Binary Tree (using Level Order Traversal - Iterative Approach)
+ //Maximum Depth or Height of Binary Tree (using Level Order Traversal - Iterative Approach)
 //Approach: Use a variable 'level', which increases by 1 on going left or right, and the highest level will always be the answer.
 
 #include<bits/stdc++.h>
@@ -21,7 +21,24 @@ class Solution {
   //TC: O(N) as we traverse all nodes.
   //SC: O(N) => for queue WC: if complete BT & last level is completely filled, queue will end up storing almost all N nodes.
   int maxDepth(TreeNode* root) {
+    int levels = 0;
+    if(root == NULL) return levels;
     
+    queue<TreeNode*> q;
+    q.push(root);
+    
+    while(!q.empty()) {
+      int size = q.size();
+      for(int i=0;i<size;i++) {
+        TreeNode* node = q.front();
+        q.pop();
+        if(node->left != NULL) q.push(node->left);
+        if(node->right != NULL) q.push(node->right);
+      }
+      levels++;
+    }
+    
+    return levels;
   }
 };
 
