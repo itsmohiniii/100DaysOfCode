539a4ec86e24410fb06e79d6d9bc4afd60ef06b4
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-08-07 12:19:11 +0530

    Update 11_Height_BT_Iterative.cpp

--- Trees/11_Height_BT_Iterative.cpp
+++ Trees/11_Height_BT_Iterative.cpp
@@ -1,1 +1,39 @@
+//Maximum Depth or Height of Binary Tree (using Level Order Traversal - Iterative Approach)
+//Approach: Use a variable 'level', which increase by 1 while going left or right, and the highest level will always be the answer.
 
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+  int data;
+  TreeNode* left;
+  TreeNode* right;
+
+  TreeNode(int val) {
+    data = val;
+    left = nullptr;
+    right = nullptr;
+  }
+ }; 
+
+class Solution {
+  public:
+  //TC: O(N) as we traverse all nodes.
+  //SC: O(N) => it takes O(H) for recursive stack space WC: if skewed tree, height of binary tree will be N, hence, SC = O(N).
+  int maxDepth(TreeNode* root) {
+    
+  }
+};
+
+int main() {
+  TreeNode* root = new TreeNode(1);
+  root->left = new TreeNode(2);
+  root->right = new TreeNode(3);
+  root->left->left = new TreeNode(4);   
+  root->left->right = new TreeNode(5); 
+  
+  Solution obj;
+  cout<<obj.maxDepth(root);  //Output: 3
+
+  return 0;
+}
