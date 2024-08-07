c1c75831141fc10ba0a71ff9751f3d9a00508a08
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-08-07 12:43:56 +0530

    Update 11_Height_BT_Iterative.cpp

--- Trees/11_Height_BT_Iterative.cpp
+++ Trees/11_Height_BT_Iterative.cpp
@@ -1,5 +1,5 @@
 //Maximum Depth or Height of Binary Tree (using Level Order Traversal - Iterative Approach)
-//Approach: Use a variable 'level', which increases by 1 on going left or right, and the highest level will always be the answer.
+//Hint: Use a variable 'level', which increases by 1 on going left or right, and the highest level will always be the answer.
 
 #include<bits/stdc++.h>
 using namespace std;
