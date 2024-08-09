cca1ac2de8d4d72ecf458b14ca14793e5297395e
itsmohiniii <74259856+itsmohiniii@users.noreply.github.com>
2024-08-09 21:09:32 +0530

    Update 16_ZigZagTraversal.cpp

--- Trees/16_ZigZagTraversal.cpp
+++ Trees/16_ZigZagTraversal.cpp
@@ -1,1 +1,76 @@
+//Zig-Zag or Spiral Traversal in Binary Tree
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+  int data;
+  TreeNode* left;
+  TreeNode* right;
+
+  TreeNode(int val): data(val), left(nullptr), right(nullptr) {}
+ }; 
+
+class Solution {
+  public: 
+  vector<vector<int>> zigzagLevelOrder (TreeNode* root) {
+    vector<vector<int>> res;
+    if(root == nullptr) {
+      return res;
+    }
+    
+    queue<TreeNode*> nodesQueue;
+    nodesQueue.push(root);
+    bool leftToRight = true;
+    
+    while(!nodesQueue.empty()) {
+      int size = nodesQueue.size();
+      vector<int> row(size);
+
+      for(int i =0;i<size;i++) {
+        TreeNode* node = nodesQueue.front();
+        nodesQueue.pop();
+        int index = leftToRight ? i : size-i-1;
+        row[index] = node->data;
+        
+        if(node->left != nullptr) {
+          nodesQueue.push(node->left);
+        }
+        if(node->right != nullptr) {
+          nodesQueue.push(node->right);
+        }
+      }
+      leftToRight = !leftToRight;
+      res.push_back(row);
+    } 
+    return res;
+  }
+};
+
+int main() {
+  TreeNode* root = new TreeNode(1);
+  root->left = new TreeNode(2);
+  root->right = new TreeNode(3);
+  root->left->left = new TreeNode(4);   
+  root->left->right = new TreeNode(5); 
+  root->left->left->left = new TreeNode(6); 
+  root->left->left->right = new TreeNode(7);   
+  
+  Solution obj;
+  vector<vector<int>> zigzagOrder = obj.zigzagLevelOrder(root);  
+  for(int i=0; i<zigzagOrder.size(); i++) {
+    for(auto it: zigzagOrder[i]) {
+      cout<<it<<" ";
+    }
+    cout<<endl;
+  }
+//   Output: 
+//   1 
+//   3 2 
+//   4 5 
+//   7 6 
+
+  return 0;
+}
+
 
